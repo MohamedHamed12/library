bf5d205f47d908d9ce86115b82fe83825499faf0
Mohamed Hamed <90472426+MohamedHamed12@users.noreply.github.com>
2026-09-22 22:13:30 +0300

    style: (#59) align Spring Boot 4 imports

--- src/main/java/io/pillopl/library/catalogue/CatalogueDatabaseConfig.java
+++ src/main/java/io/pillopl/library/catalogue/CatalogueDatabaseConfig.java
@@ -4,8 +4,8 @@ import javax.sql.DataSource;
 
 import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.boot.jdbc.autoconfigure.JdbcConnectionDetails;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
